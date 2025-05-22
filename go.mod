d34fcc40637d668cd635bbaa016bb723fdf0d97c
Francesco Casetta <francesco.casetta@gmail.com>
2025-05-22 07:04:45 +0200

    feat: start server

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mitchan/chirpy
+
+go 1.24.3
---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import "net/http"
+
+func main() {
+	handler := http.NewServeMux()
+
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: handler,
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
