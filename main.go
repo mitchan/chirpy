64beff9e6fbd9732ed07ce1252d3eef54a1edddc
Francesco Casetta <francesco.casetta@gmail.com>
2025-05-23 08:38:46 +0200

    feat: custom handlers

--- main.go
+++ main.go
@@ -1,17 +1,26 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
 	handler := http.NewServeMux()
 
-	handler.Handle("/", http.FileServer(http.Dir(".")))
-	handler.Handle("/assets", http.FileServer(http.Dir("./assets")))
+	handler.Handle("/app", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	handler.Handle("/app/assets", http.StripPrefix("/app/assets", http.FileServer(http.Dir("./assets"))))
+	handler.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(200)
+		w.Write([]byte("OK"))
+	})
 
 	server := http.Server{
 		Addr:    ":8080",
 		Handler: handler,
 	}
+	fmt.Println("Server running on :8080")
 	err := server.ListenAndServe()
 	if err != nil {
 		panic(err)
