5a0c890b5841bf35d5954119d5ffae482ab44228
Francesco Casetta <francesco.casetta@gmail.com>
2025-05-25 11:05:12 +0200

    feat: stateful handler

--- main.go
+++ main.go
@@ -3,22 +3,52 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync/atomic"
 )
 
-func main() {
-	handler := http.NewServeMux()
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
-	handler.Handle("/app", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
-	handler.Handle("/app/assets", http.StripPrefix("/app/assets", http.FileServer(http.Dir("./assets"))))
-	handler.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+func (cfg *apiConfig) metricsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())))
+}
+
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hits reset to 0"))
+}
+
+func main() {
+	apiCfg := apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
+
+	mux := http.NewServeMux()
+
+	mux.Handle("/app/", apiCfg.metricsMiddleware(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
+	mux.Handle("/app/assets", http.StripPrefix("/app/assets", http.FileServer(http.Dir("./assets"))))
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(200)
 		w.Write([]byte("OK"))
 	})
+	mux.HandleFunc("/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("/reset", apiCfg.handlerReset)
 
 	server := http.Server{
 		Addr:    ":8080",
-		Handler: handler,
+		Handler: mux,
 	}
 	fmt.Println("Server running on :8080")
 	err := server.ListenAndServe()
